6f5965c21a587840ee25d3a34718b5854945abd8
benjidial <benji3.141@gmail.com>
2015-09-29 21:05:47 -0400

    Starting on v2.0.0.

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# Say
+## Say
 This outputs text at a specifed rate.  It is licensed under CC0 1.0 Universal.  If there is not a file named `License.txt` in this directory, please go to <https://creativecommons.org/publicdomain/zero/1.0/>.  
-**Command Line:**  `Say interval text` or `Say interval /f [/s] file`.  Type `Say` for more information.  
-**Return Values:**  0 = OK, -1 = Incorrect number of arguments, -2 = Invalid `interval` argument, -3 = No `file` argument, -4 = Could not find `file`, -5 = `file` has an invalid path, -6 = `file` has an invalid name, -7 = Unexpected I/O error reading from `file`.
-
-# Version History:
-## 1.x.x
+## Command Line:
+`Say --help`  
+`Say --license`  
+`Say --version`  
+`Say [-i=interval] -f=filename`  
+`Say [-i=interval] -t=text`
+## Return Values:
+* 0: Success
+* 1: Coming soon
+* 2: Invalid arguments
+## Version History:
+### 2.x.x
+#### Say 2.0.0
+Complete redesign
+### 1.x.x
 #### Say 1.1.0
 Added support for reading from files.  
 Added capability to support other languages in the future.
---
+++ src/Say.c
@@ -0,0 +1,91 @@
+// Say v2.0.0, <www.github.com/benjidial/say>
+
+#include <stdio.h>
+#include <time.h>
+#define VERSION v2.0.0
+
+enum exitCodes
+{
+  SUCCESS = 0,
+  COMING_SOON = 1,
+  INVALID_ARGUMENTS = 2
+}
+
+void comingSoon()
+{
+  printf("This capability is coming soon!\n");
+  exit(COMING_SOON);
+}
+
+void invArgs(char *message)
+{
+  fprintf(stderr, "Invalid arguments!\n%s\nDo 'Say --help' for help.\n", message);
+  exit(INVALID_ARGUMENTS);
+}
+
+void help()
+{
+  printf("Say syntax:\n\n");
+  printf("Say --help\n");
+  printf("Say --license\n");
+  printf("Say --version\n");
+  printf("Say [-i=interval] -f=filename\n");
+  printf("Say [-i=interval] -t=text\n\n");
+  printf("  --help     Prints this screen\n");
+  printf("  --license  Prints lincense information\n");
+  printf("  --version  Provides version and project info\n\n");
+  printf("  interval   The time in milliseconds between each character\n");
+  printf("             If not specified, 750 ms is used.\n");
+  printf("  filename   A file the contents of which to output\n");
+  printf("  text       Text to output\n\n\n");
+  printf("Return values of Say:\n\n");
+  printf("0:   Success\n");
+  printf("1:   Coming soon\n");
+  printf("2:   Invalid arguments\n");
+  exit(SUCCESS);
+}
+
+void license()
+{
+  printf("Say is licensed under CC0 1.0 Universal.\n");
+  printf("You should have gotten a file named 'License.txt' with Say.\n");
+  printf("For more information, visit <creativecommons.org/publicdomain/zero/1.0>.\n");
+  exit(SUCCESS);
+}
+
+void version()
+{
+  printf("Say, VERSION.\n"); // See #define near the top of this file.
+  printf("<github.com/benjidial/say>\n");
+  exit(SUCCESS);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc == 1)
+    invArgs("Say must have at least one argument.");
+  if (argc == 2)
+  {
+    if (argv[1][0] == '-')
+    switch (argv[1][1])
+    {
+      case '-':
+        if (!strcmp(argv[1], "--help"))
+          help();
+        if (!strcmp(argv[1], "--license"))
+          license();
+        if (!strcmp(argv[1], "--version"))
+          version();
+        invArgs(sprintf("I don't recognize %s in this context.", argv[1]));
+      case 'f':
+        comingSoon();
+      case 'i':
+        comingSoon();
+      case 't':
+        comingSoon();
+      default:
+        comingSoon();
+    }
+  }
+  comingSoon();
+}
